a5a9d0ee3390d64870532c4f9515f9b8a8dbbb9c
deivezera <dzabroski@gmail.com>
2025-07-05 22:36:51 -0300

    first commit ejs files

--- server.js
+++ server.js
@@ -9,11 +9,20 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Local Server Information
@@ -27,4 +36,8 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+app.get('/', function(req,res){
+  res.render('index', {title: 'Home'})
 })
