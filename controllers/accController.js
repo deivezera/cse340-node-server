a6ec916b16404721194ce3f0a609dc286c7a0200
deivezera <dzabroski@gmail.com>
2025-07-29 20:24:35 -0300

    feat: registration view

--- controllers/accController.js
+++ controllers/accController.js
@@ -15,4 +15,13 @@ async function buildLogin(req, res, next) {
   }
 }
 
-module.exports = { buildLogin }
+async function buildRegister(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null
+  })
+}
+
+module.exports = { buildLogin, buildRegister }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,5 +3,6 @@ const router = new express.Router()
 const accController = require("../controllers/accController")
 
 router.get("/login", accController.buildLogin);
+router.get("/register", accController.buildRegister)
 
 module.exports = router;
